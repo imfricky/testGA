3c6d1643aecdf9e07cf1eecb87452ca874126f20
Rahul singh <frickyrathore02@gmail.com>
2021-05-14 00:49:39 +0530

    First

---
+++ src/main/java/com/example/TestGA/TestGaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestGA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGaApplication.class, args);
+	}
+
+}
